bcc751f4e1104cadb3553831f85bb807dcc63bbf
hendrism <hendrism@gmail.com>
2026-03-20 08:17:29 -0400

    feat: Add new vocabulary sorting activity, refactor image assets into categorized directories, and update related HTML and data files.

---
+++ migrate_images.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# Migrate from category_images
+mv category_images/set1_00_rain-boots.png images/clothing/rain-boots.png
+mv category_images/set1_01_shorts.png images/clothing/shorts.png
+mv category_images/set1_02_jacket.png images/clothing/jacket.png
+mv category_images/set1_03_scarf.png images/clothing/scarf.png
+mv category_images/set1_04_t-shirt.png images/clothing/t-shirt.png
+mv category_images/set1_05_winter-boots.png images/clothing/winter-boots.png
+
+mv category_images/set2_00_butterfly.png images/animals/butterfly.png
+mv category_images/set2_01_robin.png images/animals/robin.png
+mv category_images/set2_02_bumblebee.png images/animals/bumblebee.png
+mv category_images/set2_03_sparrow.png images/animals/sparrow.png
+mv category_images/set2_04_ant.png images/animals/ant.png
+mv category_images/set2_05_blue-jay.png images/animals/blue-jay.png
+
+mv category_images/set3_00_rose.png images/plants/rose.png
+mv category_images/set3_01_tree.png images/plants/tree.png
+mv category_images/set3_02_rock.png images/nature/rock.png
+mv category_images/set3_03_grass.png images/nature/grass.png
+mv category_images/set3_04_bench.png images/objects/bench.png
+mv category_images/set3_05_cloud.png images/nature/cloud.png
+
+mv category_images/set4_00_umbrella.png images/objects/umbrella.png
+mv category_images/set4_01_sunglasses.png images/clothing/sunglasses.png
+mv category_images/set4_02_puddle.png images/nature/puddle.png
+mv category_images/set4_03_beach-ball.png images/objects/beach-ball.png
+mv category_images/set4_04_raincoat.png images/clothing/raincoat.png
+mv category_images/set4_05_kite.png images/objects/kite.png
+
+mv category_images/set5_00_chick.png images/animals/chick.png
+mv category_images/set5_01_puppy.png images/animals/puppy.png
+mv category_images/set5_02_duckling.png images/animals/duckling.png
+mv category_images/set5_03_lamb.png images/animals/lamb.png
+mv category_images/set5_04_chicken.png images/animals/chicken.png
+mv category_images/set5_05_beagle.png images/animals/beagle.png
+
+mv category_images/set6_00_bee.png images/animals/bee.png
+mv category_images/set6_01_robin.png images/animals/robin2.png
+mv category_images/set6_02_squirrel.png images/animals/squirrel.png
+mv category_images/set6_03_butterfly.png images/animals/butterfly2.png
+mv category_images/set6_04_crow.png images/animals/crow.png
+mv category_images/set6_05_rabbit.png images/animals/rabbit.png
+
+mv category_images/set7_00_sandals.png images/clothing/sandals.png
+mv category_images/set7_01_apple.png images/food/apple.png
+mv category_images/set7_02_jump-rope.png images/objects/jump-rope.png
+mv category_images/set7_03_windbreaker.png images/clothing/windbreaker.png
+mv category_images/set7_04_ice-cream-cone.png images/food/ice-cream-cone.png
+mv category_images/set7_05_soccer-ball.png images/objects/soccer-ball.png
+
+mv category_images/set8_00_cow.png images/animals/cow.png
+mv category_images/set8_01_lion.png images/animals/lion.png
+mv category_images/set8_02_golden-retriever.png images/animals/golden-retriever.png
+mv category_images/set8_03_pig.png images/animals/pig.png
+mv category_images/set8_04_elephant.png images/animals/elephant.png
+mv category_images/set8_05_cat.png images/animals/cat.png
+
+mv category_images/set9_00_rain-cloud.png images/nature/rain-cloud.png
+mv category_images/set9_01_rose.png images/plants/rose2.png
+mv category_images/set9_02_frog.png images/animals/frog.png
+mv category_images/set9_03_sun.png images/nature/sun.png
+mv category_images/set9_04_tree.png images/plants/tree2.png
+mv category_images/set9_05_bluebird.png images/animals/bluebird.png
+
+mv category_images/set10_00_cloud.png images/nature/cloud2.png
+mv category_images/set10_01_grass.png images/nature/grass2.png
+mv category_images/set10_02_fish.png images/animals/fish.png
+mv category_images/set10_03_airplane.png images/objects/airplane.png
+mv category_images/set10_04_rock.png images/nature/rock2.png
+mv category_images/set10_05_duck.png images/animals/duck.png
+
+# Migrate from images/seasons/spring (new unique user additions)
+mv images/seasons/spring/backpack.png images/objects/backpack.png
+mv images/seasons/spring/baseballglove.png images/objects/baseballglove.png
+mv images/seasons/spring/basket.png images/objects/basket.png
+mv images/seasons/spring/birdnest.png images/nature/birdnest.png
+mv images/seasons/spring/broom.png images/objects/broom.png
+mv images/seasons/spring/chalk.png images/objects/chalk.png
+mv images/seasons/spring/chipmunk.png images/animals/chipmunk.png
+mv images/seasons/spring/daffodil.png images/plants/daffodil.png
+mv images/seasons/spring/daisy.png images/plants/daisy.png
+mv images/seasons/spring/drone.png images/objects/drone.png
+mv images/seasons/spring/duster.png images/objects/duster.png
+mv images/seasons/spring/earthworm.png images/animals/earthworm.png
+mv images/seasons/spring/garden-gloves.png images/clothing/garden-gloves.png
+mv images/seasons/spring/groundhog.png images/animals/groundhog.png
+mv images/seasons/spring/icicle.png images/nature/icicle.png
+mv images/seasons/spring/larvae.png images/animals/larvae.png
+mv images/seasons/spring/mole.png images/animals/mole.png
+mv images/seasons/spring/paper_airplane.png images/objects/paper_airplane.png
+mv images/seasons/spring/picnicbasket.png images/objects/picnicbasket.png
+mv images/seasons/spring/rake.png images/objects/rake.png
+mv images/seasons/spring/sapling.png images/plants/sapling.png
+mv images/seasons/spring/shovel.png images/objects/shovel.png
+mv images/seasons/spring/snow.png images/nature/snow.png
+mv images/seasons/spring/snowman.png images/objects/snowman.png
+mv images/seasons/spring/strawberry.png images/food/strawberry.png
+mv images/seasons/spring/sun_hat.png images/clothing/sun_hat.png
+mv images/seasons/spring/swing.png images/objects/swing.png
+mv images/seasons/spring/tomato.png images/food/tomato.png
+mv images/seasons/spring/trowel.png images/objects/trowel.png
+mv images/seasons/spring/tulip.png images/plants/tulip.png
+mv images/seasons/spring/vase.png images/objects/vase.png
+mv images/seasons/spring/wasp.png images/animals/wasp.png
+mv images/seasons/spring/waterbottle.png images/objects/waterbottle.png
+mv images/seasons/spring/watering-can.png images/objects/watering-can.png
+
+# And the crops
+mv images/seasons/spring/seeds.png images/plants/seeds.png
+mv images/seasons/spring/seedling.png images/plants/seedling.png
+
+# Delete images/seasons/spring AND category_images
+rm -rf images/seasons/spring
+rm -rf category_images
